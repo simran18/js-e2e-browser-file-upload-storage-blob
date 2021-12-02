b786a387ff6516e124187c7f395acc627986fc6c
Simran Bhatia <simran18@uw.edu>
2021-12-02 09:35:06 -0800

    update packages and logo

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import logo from "./images/logo_2.gif";
+import logo from "./images/logo_3.gif";
 
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,11 +8,13 @@ function Home() {
 
   return (
     <div>
+      <div className='logo'> <img src={logo} alt="logo" /> </div>
+      
       <h2> Welcome! </h2>
-        <p>The mission of this initiative is to help people stay physically and mentally fit while staying safe during the unprecedented times of the pandemic and beyond, by improving the online yoga practice experience.</p>
+       <p> This is an innovative deep learning AI solution deployed on Azure cloud that elevates virtual yoga practice and makes it more accessible by making the video content searchable, providing the analysis of health benefits of the session and curating personalized recommendations, while reducing costs. To enable this experience, we developed a platform for multi-modal yoga video summarization powered by computer vision AI. It incorporates a user-friendly UI, a content analysis pipeline running behind the scene, and a recommendation engine that connects the two pieces together. We invested significant effort in providing privacy guarantees.</p>
+       
        <br />
         <p>The switch to remote work, lesser physical activities and social isolation have created new challenges. With the increased level of stress and loss of jobs worldwide, the development of affordable, safe, and efficient ways to maintain physical and mental health has become more important. Yoga is a great way to achieve this. However, since the beginning of the pandemics, fitness centers and yoga studios around the world have been either fully or partially unavailable due to distancing restrictions. The practitioner’s ability to receive guidance from professional instructors has significantly reduced. Navigating the ocean of online sessions published by fitness centers and independent instructors is challenging, especially for beginners and people with injuries or other health issues. In our capstone project we set a goal to make the growing amount of online video content for at-home yoga practice more accessible and searchable. Our work will empower instructors, their students, and online fitness platforms to build personalized and balanced program of exercises with AI-powered tools for summarization of videos and recommendation engine for picking practices that best match the user’s needs.  .</p>
-        <img src={logo} alt="logo" />
     </div>
   );
 }
